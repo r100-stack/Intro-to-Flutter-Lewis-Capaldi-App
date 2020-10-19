d3802a47d210362d8df7baa1346a13cdffa441eb
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-18 20:28:36 -0500

    Added solution and extra TODO for Step3

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   // TODO (3): Create a final String variable called imageUrl
   // TODO (4): imageUrl = <url of Lewis Capaldi's picture>
+  final String imageUrl =
+      'https://scontent-atl3-2.xx.fbcdn.net/v/t1.0-9/52629675_2184456524910238_9191260297254404096_o.jpg?_nc_cat=1&_nc_sid=09cbfe&_nc_ohc=a9F9vIXkdHUAX8LCRWg&_nc_ht=scontent-atl3-2.xx&oh=ac44a6b51acf18d56479eef5a5c3cf0a&oe=5FB0864A';
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +29,19 @@ class HomeScreen extends StatelessWidget {
           // TODO (5): Use imageUrl as the image url for NetworkImage
           // TODO (6): Wrap the Image with ClipRRect
           // TODO (7): ClipRRect.borderRadius = BorderRadius.circular(20.0)
+
+          // TODO (8): Add a SizedBox of height 40.0 between the text and the image
+          const SizedBox(
+            height: 40.0,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image(
+              height: 200.0,
+              width: 200.0,
+              image: NetworkImage(imageUrl),
+            ),
+          ),
         ],
       ),
     );
