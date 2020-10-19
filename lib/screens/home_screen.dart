e757be6858e1c6f97cd003c855d544aa5df7feec
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-18 20:22:47 -0500

    Removed old TODOs

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,21 +3,6 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO (1): Replace the Container with Scaffold
-    // TODO (2): Scaffold.backgroundColor = Colors.black
-
-    // TODO (3): Add a Column as the "body" of Scaffold
-
-    // Within the column's children
-    // TODO (4): Add a Text widget to display '"DIVINELY UNINSPIRED TO A HELLISH EXTENT"'
-    // TODO (5): Add a suitable TextStyle for the Text().
-    // fontSize: 20.0
-    // fontWeight: FontWeight.w900
-    // color: Colors.white
-
-    // TODO (6): Column's mainAxisAlignment = MainAxisAlignment.center
-    // TODO (7): Column's crossAxisAlignment = CrossAxisAlignment.center
-
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
